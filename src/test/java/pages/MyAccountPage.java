cbe21fb497dc94d48f0b32865c3b664faaf1fc53
Emre SOYDEMIR <eosoydemir@gmail.com>
2023-05-13 14:18:29 +0200

    Shipping And Billing Addresses Information inputs page

---
+++ src/test/java/pages/MyAccountPage.java
@@ -0,0 +1,109 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+
+public class MyAccountPage {
+    public MyAccountPage() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Emre soydemir 71-120
+    @FindBy(xpath = "//div[@class='account']")    public WebElement accountButton;
+    @FindBy(xpath = "//a[@href='signup']")    public WebElement signUpButton;
+    @FindBy(xpath = "//a[@href='signin']")    public WebElement accountsignInButton;
+     @FindBy(xpath = "//button[@name='login']")  public WebElement signInloginButton;
+    @FindBy(id = "username")    public WebElement usernameSignInInput;
+    @FindBy(id = "password")    public WebElement passwordSignInInput;
+    @FindBy(xpath = "//button[@name='login']")    public WebElement signInButton;
+    @FindBy(xpath = "(//div[@class='icon-box text-center'])[3]")    public WebElement adressLink;
+    @FindBy(id = "billing_first_name")    public WebElement addFirstName;
+    @FindBy(id = "billing_last_name")    public WebElement addLastName;
+    @FindBy(id = "billing_country")    public WebElement selectBillingCountyrDdm;
+    @FindBy(id = "billing_address_1")    public WebElement addBillingAddress;
+    @FindBy(id = "billing_city")    public WebElement addBillingCity;
+    @FindBy(id = "billing_state")    public WebElement addBillingState;
+    @FindBy(id = "billing_postcode")    public WebElement addBillingPostcode;
+    @FindBy(id = "billing_phone")    public WebElement addBillingPhone;
+    @FindBy(id = "billing_email")    public WebElement addBillingEmail;
+    @FindBy(css = "#main > div > div > div > div > div > div > div > form > div > p > button")    public WebElement saveAddressButton;
+    @FindBy(xpath = "//*[@id=\"main\"]/div/div/div/div/div/div/div/div[1]")    public WebElement savedBillingAddressAllert;
+    @FindBy(id = "shipping_first_name") public WebElement addShippingFirstName;
+    @FindBy(id = "shipping_last_name") public WebElement addShippingLastName;
+    @FindBy(id = "shipping_country")    public WebElement selectShippingCountyrDdm;
+    @FindBy(id = "shipping_address_1")    public WebElement addShippingAddress;
+    @FindBy(id = "shipping_city")    public WebElement addShippingCity;
+    @FindBy(id = "shipping_postcode")    public WebElement addShippingPostcode;
+    @FindBy(id = "shipping_email")    public WebElement addShippingEmail;
+    @FindBy(xpath = "//*[@id=\"main\"]/div/div/div/div/div/div/div/div[1]/div")    public WebElement savedshippingAddressAllert;
+
+
+
+
+
+
+
+
+
+
+}
