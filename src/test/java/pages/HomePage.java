4496eb8a77a0b7597f1c6ce4bb8ef41be8f3b314
ERELOZTURK <erelozturk2@gmail.com>
2023-05-13 21:28:11 +0300

    13:05.2023

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -9,20 +9,14 @@ public class HomePage {
     public HomePage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@FindBy(css = "*[class='login inline-type'] ") public WebElement register;
+    @FindBy(xpath = "//*[@href='signup']" ) public WebElement signup;
+    @FindBy(xpath = "//*[@id='reg_username']" ) public WebElement username;
+    @FindBy(xpath = "//*[@type='email']" ) public WebElement email;
+    @FindBy(xpath = "//*[@id='reg_password']" ) public WebElement password;
+    @FindBy(xpath = "//*[@for='register-policy']" ) public WebElement iAgree;
+    @FindBy(xpath = "//*[@value='Sign Up']" ) public WebElement signupButon;
+    @FindBy(xpath = "//*[text()='An account is already registered with your email address. ']" ) public WebElement alreadyRegister;
 
 
 
--- src/test/java/tests/US05_Aykut.java
+++ src/test/java/tests/US05_Aykut.java
@@ -1,14 +1,16 @@
 package tests;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.MyAccount;
+import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ReusableMethods;
 
 public class US05_Aykut {
-    public US05_Aykut(){
-        PageFactory.initElements(Driver.getDriver(),this);
-    }
-
 
 
 }
