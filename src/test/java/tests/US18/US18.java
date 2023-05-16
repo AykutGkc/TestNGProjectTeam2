3611af1fe103eb7aa13201e78ea5266bdbdb352c
murat <svsmurat358@gmail.com>
2023-05-16 21:03:40 +0200

    murat_us18_20

--- src/test/java/tests/US18/US18.java
+++ src/test/java/tests/US18/US18.java
@@ -2,7 +2,9 @@ package tests.US18;
 
 import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
-import pages.MyAccountMurat;
+import pages.HomePage;
+
+import pages.MyAccountPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 
@@ -11,15 +13,16 @@ public class US18 {
     @Test
     public void test18() {
         Driver.getDriver().get(ConfigReader.getProperty("perarlyMRK"));
-        MyAccountMurat myAccountMurat= new MyAccountMurat();
-        myAccountMurat.signIn.click();
-        myAccountMurat.username.sendKeys("elchonon.radek@fullangle.org"+ Keys.ENTER);
-        myAccountMurat.password.sendKeys("zgaxv5V@"+Keys.ENTER);
-        myAccountMurat.tikla.click();
-        myAccountMurat.signOut.click();
-        myAccountMurat.storeManager.click();
-        myAccountMurat.products.click();
-        myAccountMurat.addNew.click();
+        MyAccountPage myAccountPage= new MyAccountPage();
+
+        myAccountPage.signIn.click();
+        myAccountPage.username.sendKeys("elchonon.radek@fullangle.org"+ Keys.ENTER);
+        myAccountPage.password.sendKeys("zgaxv5V@"+Keys.ENTER);
+        myAccountPage.tikla.click();
+        myAccountPage.signOut.click();
+        myAccountPage.storeManager.click();
+        myAccountPage.products.click();
+        myAccountPage.addNew.click();
 
 
     }
--- src/test/java/tests/US20/US20.java
+++ src/test/java/tests/US20/US20.java
@@ -2,7 +2,8 @@ package tests.US20;
 
 import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
-import pages.MyAccountMurat;
+
+import pages.MyAccountPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 
@@ -11,20 +12,20 @@ public class US20 {
     public void testUS20() {
 
         Driver.getDriver().get(ConfigReader.getProperty("perarlyMRK"));
-        MyAccountMurat myAccountMurat= new MyAccountMurat();
-        myAccountMurat.signIn.click();
-        myAccountMurat.username.sendKeys("mansur.lakeland@fullangle.org"+ Keys.ENTER);
-        myAccountMurat.password.sendKeys("gmbnn8C@"+Keys.ENTER);
-        myAccountMurat.tikla.click();
-        myAccountMurat.signOut.click();
-        myAccountMurat.storeManager.click();
-        myAccountMurat.Coupons.click();
-        myAccountMurat.addNew.click();
-        myAccountMurat.title.sendKeys("1234");
-        myAccountMurat.descrption.sendKeys("yeni kod");
-        myAccountMurat.discount_type.sendKeys(""+ Keys.ENTER);
-        myAccountMurat.coupon_amount.sendKeys("120");
-        myAccountMurat.expiry_date.sendKeys("2023-12-12");
+        MyAccountPage myAccountPage= new MyAccountPage();
+        myAccountPage.signIn.click();
+        myAccountPage.username.sendKeys("mansur.lakeland@fullangle.org"+ Keys.ENTER);
+        myAccountPage.password.sendKeys("gmbnn8C@"+Keys.ENTER);
+        myAccountPage.tikla.click();
+        myAccountPage.signOut.click();
+        myAccountPage.storeManager.click();
+        myAccountPage.Coupons.click();
+        myAccountPage.addNew.click();
+        myAccountPage.title.sendKeys("1234");
+        myAccountPage.descrption.sendKeys("yeni kod");
+        myAccountPage.discount_type.sendKeys(""+ Keys.ENTER);
+        myAccountPage.coupon_amount.sendKeys("120");
+        myAccountPage.expiry_date.sendKeys("2023-12-12");
 
 
 
